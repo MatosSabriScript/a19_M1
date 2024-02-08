372808a3697a2171dd38f8ddbec6db9939cf219e
Sabrina Matos <sabrinamatosalmeida2@gmail.com>
2024-02-08 10:37:05 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var nomes= ["Sarah", "Sabrina", "Daniel",]
+console.log(nomes)
+
+
+nomes.push("Luna")
+console.log(nomes)
+
+
+nomes.unshift("Simon")
+console.log(nomes)
+
+nomes.pop()
+console.log(nomes)
+
+nomes.pop()
+console.log(nomes)
+
+nomes.push("Luna")
+console.log(nomes)
+
